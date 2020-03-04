3f24d9957f1c9019cc097faacf69d109bbd443c3
Ivan Kalinin <ivan_kalinin1@epam.com>
2020-03-04 17:23:13 +0300

    Add Vividus test site (#1)

---
+++ src/test/kotlin/org/vividus/testsite/TestSiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.vividus.testsite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestSiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
