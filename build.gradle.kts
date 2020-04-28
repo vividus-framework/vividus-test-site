50c5fecc638e393cf4d276794dd1d7d57a3392d0
Valery Yatsynovich <vyatsyno@its.jnj.com>
2020-04-28 18:17:30 +0300

    Apply Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.0.M1"
-	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.70"
-	kotlin("plugin.spring") version "1.3.70"
+    id("org.springframework.boot") version "2.3.0.M1"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("com.diffplug.gradle.spotless") version "3.28.1"
+    kotlin("jvm") version "1.3.70"
+    kotlin("plugin.spring") version "1.3.70"
 }
 
 group = "org.vividus"
@@ -12,27 +14,42 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+configure<SpotlessExtension> {
+    format("resources") {
+        target("**/*.md", "**/*.html")
+        indentWithSpaces()
+        endWithNewline()
+    }
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
 }
--- src/main/kotlin/org/vividus/testsite/TestSiteApplication.kt
+++ src/main/kotlin/org/vividus/testsite/TestSiteApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class TestSiteApplication
 
 fun main(args: Array<String>) {
-	runApplication<TestSiteApplication>(*args)
+    runApplication<TestSiteApplication>(*args)
 }
