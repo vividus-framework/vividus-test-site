eb52bde00506e939a575901b44320956022f8a2f
Valery Yatsynovich <valery_yatsynovich@epam.com>
2020-04-28 17:21:19 +0300

    Upgrade to Kotlin 1.3.70 (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.3.0.M1"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.61"
-	kotlin("plugin.spring") version "1.3.61"
+	kotlin("jvm") version "1.3.70"
+	kotlin("plugin.spring") version "1.3.70"
 }
 
 group = "org.vividus"
