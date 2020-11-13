90f55f146f36296e566326cf147faf8e3a1b3837
Valery Yatsynovich <valery_yatsynovich@epam.com>
2020-11-13 13:24:53 +0300

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.0.M1"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id("com.diffplug.gradle.spotless") version "3.28.1"
-    kotlin("jvm") version "1.3.70"
-    kotlin("plugin.spring") version "1.3.70"
+    id("org.springframework.boot") version "2.4.0"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("com.diffplug.spotless") version "5.8.1"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
 }
 
 group = "org.vividus"
@@ -15,18 +15,15 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.github.ben-manes.caffeine:caffeine:2.8.4")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.8.6")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
