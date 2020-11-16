1f27e9640d847bba3a1c52477edfd589a3245358
Valery Yatsynovich <valery_yatsynovich@epam.com>
2020-11-16 11:30:21 +0300

    Add REST endpoint for HTTP retry by status code (#25)

--- src/main/kotlin/org/vividus/testsite/api/HttpRequestController.kt
+++ src/main/kotlin/org/vividus/testsite/api/HttpRequestController.kt
@@ -2,30 +2,46 @@ package org.vividus.testsite.api
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.LoadingCache
+import java.time.Duration
 import java.util.UUID
-import java.util.concurrent.TimeUnit
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController()
+@RequestMapping("/api")
 class HttpRequestController {
-    val cache: LoadingCache<UUID, Long> = Caffeine.newBuilder()
-            .expireAfterWrite(10, TimeUnit.SECONDS)
-            .build { System.currentTimeMillis() }
+    private val CACHE_EVICTION_TIME = Duration.ofSeconds(10)
+    private val HALF_CACHE_EVICTION_TIME = CACHE_EVICTION_TIME.toMillis() / 2
 
-    @GetMapping("/api/delayed-response")
+    val delayedResponseCache: LoadingCache<UUID, Long> = buildCache { System.currentTimeMillis() }
+    val teapotInvocations: LoadingCache<UUID, Long> = buildCache { 0 }
+
+    private fun buildCache(loader: (key: UUID) -> Long): LoadingCache<UUID, Long> {
+        return Caffeine.newBuilder().expireAfterWrite(CACHE_EVICTION_TIME).build(loader)
+    }
+
+    @GetMapping("/delayed-response")
     fun delayResponse(@RequestParam clientId: UUID): ResponseEntity<Unit> {
-        val firstRequestTimestamp = cache.get(clientId)
-        if (firstRequestTimestamp != null && countAgeInSeconds(firstRequestTimestamp) < 5) {
+        val firstRequestTimestamp = delayedResponseCache.get(clientId)
+        if (System.currentTimeMillis() - firstRequestTimestamp!! < HALF_CACHE_EVICTION_TIME) {
             return ResponseEntity.notFound().build()
         }
-        cache.invalidate(clientId)
+        delayedResponseCache.invalidate(clientId)
         return ResponseEntity.ok().build()
     }
 
-    private fun countAgeInSeconds(firstRequestTimestamp: Long): Long {
-        return (System.currentTimeMillis() - firstRequestTimestamp) / 1000
+    @GetMapping("/teapot")
+    fun teapot(@RequestParam clientId: UUID): ResponseEntity<Unit> {
+        val numberOfInvocations = teapotInvocations.get(clientId)
+        if (numberOfInvocations!! == 0L) {
+            teapotInvocations.put(clientId, numberOfInvocations.inc())
+            return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build()
+        }
+        teapotInvocations.invalidate(clientId)
+        return ResponseEntity.ok().build()
     }
 }
