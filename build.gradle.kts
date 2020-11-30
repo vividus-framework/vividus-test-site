04c264c8c2638ecf57ff903043c6e67dc10ccd6e
Valery Yatsynovich <valery_yatsynovich@epam.com>
2020-11-30 11:14:48 +0300

    Bump Spotless from 5.8.1 to 5.8.2 (#27)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.0"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("com.diffplug.spotless") version "5.8.1"
+    id("com.diffplug.spotless") version "5.8.2"
     kotlin("jvm") version "1.4.20"
     kotlin("plugin.spring") version "1.4.20"
 }
