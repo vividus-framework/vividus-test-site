c008a29294a997fbde919c4fdfa48dc35e43be42
Valery Yatsynovich <valery_yatsynovich@epam.com>
2020-12-21 17:26:22 +0300

    Bump Caffeine from 2.8.6 to 2.8.8 (#30)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.github.ben-manes.caffeine:caffeine:2.8.6")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.8.8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
