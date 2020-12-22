7947e8547e0f80b7fe2a5e242bdae5b09f53faf3
Ivan Kalinin <ivan_kalinin1@epam.com>
2020-12-22 23:09:30 +0300

    Add page with a delayed loading

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("com.github.ben-manes.caffeine:caffeine:2.8.8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
