70c95d80a516a8cc2d0a0b6d638dcb894cbc878b
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-05-07 00:29:58 +0300

    Bump Spring dependency management from 1.0.10.RELEASE to 1.0.11.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.4.5"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.diffplug.spotless") version "5.12.4"
     kotlin("jvm") version "1.5.0"
     kotlin("plugin.spring") version "1.5.0"
