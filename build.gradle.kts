ae31e6c578abf49c1ae9ebe8c527c68766299f6f
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-09-01 15:53:15 +0300

    Disable generation of plain JAR
    https://docs.spring.io/spring-boot/docs/current/gradle-plugin/reference/htmlsingle/#packaging-executable.and-plain-archives
    https://stackoverflow.com/questions/67663728/spring-boot-2-5-0-generates-plain-jar-file-can-i-remove-it

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
