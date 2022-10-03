22e64382ebb75845ec39eaa7ec94c14dcdfc02a8
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-10-04 01:33:43 +0300

    Bump Kotlin from 1.7.10 to 1.7.20 (#143)
    Bumps [jvm](https://github.com/JetBrains/kotlin) from 1.7.10 to 1.7.20.
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/v1.7.20/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v1.7.10...v1.7.20)
    
    Bumps [plugin.spring](https://github.com/JetBrains/kotlin) from 1.7.10 to 1.7.20.
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/v1.7.20/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v1.7.10...v1.7.20)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     id("org.springframework.boot") version "2.7.4"
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
     id("com.diffplug.spotless") version "6.11.0"
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.spring") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.spring") version "1.7.20"
 }
 
 group = "org.vividus"
