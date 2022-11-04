98771b010df77b6854cd5dd9e6fa3b2b5d14ae80
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-11-04 16:19:12 +0300

    Publish docker image to Docker Hub

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.diffplug.spotless") version "6.11.0"
+    id("com.bmuschko.docker-spring-boot-application") version "8.1.0"
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.spring") version "1.7.20"
 }
@@ -56,5 +57,13 @@ configure<SpotlessExtension> {
     kotlinGradle {
         target("*.gradle.kts")
         ktlint()
+    }
+}
+
+docker {
+    springBootApplication {
+        baseImage.set("eclipse-temurin:17-jre")
+        maintainer.set("VIVIDUS Team \"vividus.team@vividus.dev\"")
+        images.set(setOf("vividus/vividus-test-site:${project.version}", "vividus/vividus-test-site:latest"))
     }
 }
