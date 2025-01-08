7c3341f39a5e8fc0643e43c3a20fae6edb3b2d97
Valery Yatsynovich <valery_yatsynovich@epam.com>
2025-01-08 19:24:03 +0300

    Fix Gradle warning: stop relying on automatic test dependencies
    https://docs.gradle.org/8.12/userguide/upgrading_version_8.html#test_framework_implementation_dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.getByName<Jar>("jar") {
